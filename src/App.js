9efa029f7cca5176d90ef955455d1a1a8111107a
z9in <113665653+z9in@users.noreply.github.com>
2023-02-16 14:02:56 +0900

    kakao_maps

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import "./App.css";
+import { Map } from "react-kakao-maps-sdk";
+import { useEffect, useState } from "react";
+const { kakao } = window;
+function App() {
+  const [positions, setPositions] = useState([
+    { lat: 35.73231364037916, lng: 127.84714600698338 },
+  ]);
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        let new_geo = [
+          {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          },
+        ];
+        setPositions(new_geo);
+        send(new_geo);
+      });
+    } else {
+      alert("no geolocation support");
+    }
+  }, []);
+  function send(position) {
+    window.ReactNativeWebView.postMessage(`
+    ${position[0].lat},
+    ${position[0].lng}
+    `);
+  }
+  return (
+    <div className="App">
+      <Map
+        center={{ lat: positions[0].lat, lng: positions[0].lng }}
+        style={{ width: "100vw", height: "100vh" }}
+        level={11}
+        onClick={(_t, mouseEvent) => {
+          console.log(mouseEvent);
+          let new_geo = [
+            {
+              lat: Math.floor(mouseEvent.latLng.getLat() * 10000) / 10000,
+              lng: Math.floor(mouseEvent.latLng.getLng() * 10000) / 10000,
+            },
+          ];
+          send(new_geo);
+        }}
+        zoomable={false}
+      ></Map>
+    </div>
+  );
+}
+
+export default App;
